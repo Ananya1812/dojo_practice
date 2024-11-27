d48ad249eee1d9fed50e82824209eff3fc343796
Ananya1812 <ananyakini04@gmail.com>
2024-11-27 22:23:53 +0530

    sort ddl added

--- 5th-belt-py/linkedlist/sortDLL.py
+++ 5th-belt-py/linkedlist/sortDLL.py
@@ -3,46 +3,43 @@ class Node:
         self.data = data
         self.prev = None
         self.next = None
-
-# Function to create a doubly linked list from an array
-def create_doubly_linked_list(arr):
-    if not arr:
+        
+def create_doubly_linked_list(lst):
+    if not lst:
         return None
-    head = Node(arr[0])
-    curr = head
-    for data in arr[1:]:
+    head = Node(lst[0])
+    current = head
+    for data in lst[1:]:
         new_node = Node(data)
-        curr.next = new_node
-        new_node.prev = curr
-        curr = new_node
+        current.next = new_node
+        new_node.prev = current
+        current = new_node
     return head
-
-# Function to print a doubly linked list
-def print_doubly_linked_list(head):
-    curr = head
-    while curr:
-        print(curr.data, end=" ")
-        curr = curr.next
-    print()
-
-# Bubble Sort for Doubly Linked List
-def bubble_sort(head):
-    if not head or not head.next:
-        return head
     
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.data,end = " ")
+        current = current.next
+    print()
+    
+def sort_linked_list(head):
+    if not head and head.next:
+        return None
     swapped = True
     while swapped:
         swapped = False
         current = head
         while current and current.next:
-            if current.data > current.next.data:
-                current.data, current.next.data = current.next.data, current.data
+            if current.data >  current.next.data:
+                current.data, current.next.data = current.next.data,current.data
                 swapped = True
             current = current.next
     return head
-
-n = int(input())  
-input_list = list(map(int, input().split()))  
+            
+n = int(input()) 
+input_list = list(map(int,input().split()))
 head = create_doubly_linked_list(input_list)
-head = bubble_sort(head)
-print_doubly_linked_list(head)
+srted_head = sort_linked_list(head)
+print_linked_list(srted_head)
+        
